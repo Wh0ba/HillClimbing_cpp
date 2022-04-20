91a256320d51ad981405d7b2dd75a761df7e61d3
Wh0ba <whappfanr@yahoo.com>
2022-04-20 16:34:34 +0300

    HillClimb func finished phase 1

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <stack>
 #include <string>
+#include <algorithm>
 using namespace std;
 struct Node {
 	char state;
@@ -67,22 +68,42 @@ void printGraph(Node parents[], Node children[]) {
 		cout << endl;
 	}
 }
+
 // 0 1 2 3 4
 // A A B C C
 // B C D E F
-string HillClimb(Node parents[], Node children[],char start, char goal){
-	stack<char> open;
-	stack<char> close;
-	for(int i=0;i<edgeCount;i++){
-		if(parents[i].state != start) continue;
-		Node c = parents[i];
-		Node minNode = children[i];
-		for(int j=i+1;j<edgeCount){
-			if(c.state == parents[i]){
-				
+string HillClimb(Node parents[], Node children[],Node start, Node goal){
+	stack<Node> open;
+	stack<Node> close;
+	open.push(start);
+	while(!open.empty()){
+		for(int i=0;i<edgeCount;i++){
+			if(parents[i].state != open.top().state) {
+				if(i == edgeCount - 1){
+					close.push(open.top());
+					open.pop();
+					break;
+				}
+				continue;
 			}
+			Node c = parents[i];
+			close.push(open.top());
+			open.pop();
+			Node minNode = children[i];
+			for(int j=i+1;j<edgeCount){
+				if(c.state == parents[i]){
+					if(children[j].value < minNode.value) minNode = children[j];
+				}		
+			}
+			open.push(minNode)
+			break;
 		}
-
 	}
-
+	string path = "";
+	while(!close.empty()){
+		path += close.top();
+		close.pop();
+	}
+	reverse(path.begin(), path.end());
+	return path;
 }
